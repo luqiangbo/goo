95ac1d9d396d778b94e46d1d5fbd9576cf04e82b
luqiangbo <luqiangbo@orionstar.com>
2022-06-27 19:53:57 +0800

    ✨ gin

--- main.go
+++ main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"goo/middlewares"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Printf("你好")
+	router := gin.Default()
+	// 跨域
+	router.Use(middlewares.Cors())
+	router.POST("/upload_chunk", func(c *gin.Context) {
+		fileHash := c.PostForm("md5")
+		file, err := c.FormFile("file")
+
+		if err != nil {
+			fmt.Println("获取文件失败", file, fileHash)
+		}
+		fmt.Println("获取文件", file, fileHash)
+		c.JSON(200, gin.H{
+			"message": "ok",
+		})
+	})
+	router.Run()
 }
