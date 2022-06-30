890d9501948661558e262b1ffb8ec5991b01fef0
luqiangbo <luqiangbo@orionstar.com>
2022-06-30 19:48:25 +0800

    提交

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"goo/middlewares"
+	"os"
 
 	"github.com/gin-gonic/gin"
 )
@@ -12,15 +13,31 @@ func main() {
 	// 跨域
 	router.Use(middlewares.Cors())
 	router.POST("/upload_chunk", func(c *gin.Context) {
-		fileHash := c.PostForm("md5")
-		file, err := c.FormFile("file")
-
-		if err != nil {
-			fmt.Println("获取文件失败", file, fileHash)
+		file_hash := c.Request.FormValue("file_hash")
+		index := c.Request.FormValue("index")
+		total := c.Request.FormValue("total")
+		file, errFile := c.FormFile("file")
+		if errFile != nil {
+			c.JSON(200, gin.H{
+				"message": "error 1",
+			})
+			return
 		}
-		fmt.Println("获取文件", file, fileHash)
+		path_hash := fmt.Sprintf("./upload/%s", file_hash)
+		os.Mkdir(path_hash, os.ModePerm)
+		errSave := c.SaveUploadedFile(file, fmt.Sprintf("./upload/%s/%s", file_hash, file.Filename))
+		if errSave != nil {
+			c.JSON(200, gin.H{
+				"message": "error 2",
+			})
+			return
+		}
+
 		c.JSON(200, gin.H{
 			"message": "ok",
+			"index":   index,
+			"total":   total,
+			"hash1":   file_hash,
 		})
 	})
 	router.Run()
